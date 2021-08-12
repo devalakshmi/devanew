21e5e442538780ce439c4f37f4b2fc1bf372c4d7
tvminfotech <78191669+tvminfotech@users.noreply.github.com>
2021-08-12 10:50:55 +0530

    seccommit

---
+++ dir1/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dir1';
+  display = true;
+  username ="raja";
+  users = [
+    {name:"deva",id:"01",age:"20"},
+    {name:"vino",id:"02",age:"21"},
+    {name:"ribi",id:"03",age:"22"},
+    {name:"rohith",id:"04",age:"23"},
+    {name:"preetha",id:"05",age:"24"}
+  ];
+}
