62b62817a952d84e4add1efe317a6b5cedef0287
tvminfotech <78191669+tvminfotech@users.noreply.github.com>
2021-08-17 17:09:58 +0530

    devalakshmisecond commit

--- dir1/src/app/app.component.ts
+++ dir1/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dir1';
+  title = 'My New Project';
   display = true;
   username ="raja";
   users = [
