53089808a4623e5acc9564bf2b1835b7c3f7274e
devalakshmi <kdevalakshmi@gmail.com>
2021-08-29 01:00:06 -0700

    deva-commit18

--- git-practice/src/app/practice-one/practice-one.component.ts
+++ git-practice/src/app/practice-one/practice-one.component.ts
@@ -10,6 +10,13 @@ title='Interpolation';
 propertybtn:boolean=true;
 twowaybinding: string = "Display binding string";
 ngifdisplay=true;
+ngfordirectives = [
+  {name:"deva",id:"01",age:"20"},
+  {name:"vino",id:"02",age:"21"},
+  {name:"ribi",id:"03",age:"22"},
+  {name:"rohith",id:"04",age:"23"},
+  {name:"preetha",id:"05",age:"24"}
+];
 
 
 demo(){
