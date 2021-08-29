86031291b9321ca44bb32476eeb85e319f80346a
devalakshmi <kdevalakshmi@gmail.com>
2021-08-28 23:43:01 -0700

    deva-commit2

---
+++ git-practice/src/app/practice-one/practice-one.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PracticeOneComponent } from './practice-one.component';
+
+describe('PracticeOneComponent', () => {
+  let component: PracticeOneComponent;
+  let fixture: ComponentFixture<PracticeOneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PracticeOneComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PracticeOneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
