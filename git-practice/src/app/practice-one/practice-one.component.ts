85b1366ece27e66a7165e850b3984248b47513de
devalakshmi <kdevalakshmi@gmail.com>
2021-08-28 23:49:13 -0700

    deva-commit4

---
+++ git-practice/src/app/practice-one/practice-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-one',
+  templateUrl: './practice-one.component.html',
+  styleUrls: ['./practice-one.component.css']
+})
+export class PracticeOneComponent implements OnInit {
+title='Interpolation';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
