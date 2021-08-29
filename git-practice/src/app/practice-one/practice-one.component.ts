0f7e7199d81723fc8b826046c7aded71a499bf31
devalakshmi <kdevalakshmi@gmail.com>
2021-08-29 02:51:44 -0700

    deva-commit32

--- git-practice/src/app/practice-one/practice-one.component.ts
+++ git-practice/src/app/practice-one/practice-one.component.ts
@@ -21,6 +21,15 @@ ngswitchusername="raja";
 attributearr=[{value:9},{value:10},{value:11},{value:12},{value:13}]
 pipebirthday = new Date(1997, 10, 25);
 
+birthday = new Date(1988, 3, 15); // April 15, 1988 -- since month parameter is zero-based
+toggle = true; // start with true == shortDate
+get format()   { 
+  return this.toggle ? 'shortDate' : 'fullDate'; 
+}
+toggleFormat() { 
+  this.toggle = !this.toggle; 
+}
+
 
 demo(){
   this.propertybtn=false;
